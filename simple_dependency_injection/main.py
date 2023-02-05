f24361080792f8af0e2dce7e1a5d02153cd00fc7
YeonwooSung <neos960518@gmail.com>
2023-02-05 18:11:39 +0900

    Add simple example for dependency injection

---
+++ simple_dependency_injection/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+
+# init app
+app = FastAPI()
+
+# async function for dependency injection
+async def common_parameters(q: str = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
