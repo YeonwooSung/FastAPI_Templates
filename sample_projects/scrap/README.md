e8a0832ac3920c281cd8f85ca87c4df160e7deae
YeonwooSung <neos960518@gmail.com>
2023-05-13 14:24:38 +0900

    feat: Add simple web scraping api server

---
+++ sample_projects/scrap/README.md
@@ -0,0 +1,12 @@
+# Scraping API server
+
+A simple Web scraping API server built with FastAPI.
+
+## Instructions
+
+0. Set up virtualenv if required.
+1. Install poetry
+2. Install lock dependencies: `poetry install`
+3. Export requirements.txt from poetry: `sh export_requirements_txt_from_poetry.sh`
+4. Install pip dependencies: `pip install -r requirements.txt`
+5. Run the server with Host and Port: `sh start.sh 0.0.0.0:8080`
---
+++ sample_projects/scrap/main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from bs4 import BeautifulSoup
+
+import httpx
+
+from exceptions import UnicornException
+from settings import Settings
+
+from log import init_log
+from cors import init_cors
+from instrumentator import init_instrumentator
+from config import Config
+
+import urllib.parse
+
+
+app = FastAPI()
+settings = Settings()
+
+conf = Config(settings.CONFIG_PATH)
+
+init_log(app, conf.section("log")["path"])
+init_cors(app)
+init_instrumentator(app)
+
+client = httpx.AsyncClient()
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=exc.status,
+        content={"code": exc.code, "message": exc.message},
+    )
+
+async def call_api(url: str):
+    r = await client.get(url)
+    return r.text
+
+
+def parse_opengraph(body: str):
+    soup = BeautifulSoup(body, 'html.parser')
+
+    title = soup.find("meta",  {"property":"og:title"})
+    url = soup.find("meta",  {"property":"og:url"})
+    og_type = soup.find("meta",  {"property":"og:type"})
+    image = soup.find("meta",  {"property":"og:image"})
+    description = soup.find("meta",  {"property":"og:description"})
+    author = soup.find("meta",  {"property":"og:article:author"})
+
+    resp = {}
+    scrap = {}
+    scrap["title"] = title["content"] if title else None
+    scrap["url"] = url["content"] if url else None
+    scrap["type"] = og_type["content"] if og_type else None
+    scrap["image"] = image["content"] if image else None
+    scrap["description"] = description["content"] if description else None
+    scrap["author"] = author["content"] if author else None
+    resp["scrap"] = scrap
+
+    return resp
+
+@app.get("/api/v1/scrap/")
+#@cache()
+async def scrap(url: str):
+    try:
+        url = urllib.parse.unquote(url)
+        body = await call_api(url)
+        return parse_opengraph(body)
+    except Exception as e:
+        raise UnicornException(status=400, code=-20000, message=str(e))
