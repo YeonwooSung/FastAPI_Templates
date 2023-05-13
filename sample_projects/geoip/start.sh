0da2d45a80b1a18f71625c1c212b9eefdb8e3588
YeonwooSung <neos960518@gmail.com>
2023-05-13 14:20:08 +0900

    fix: Fix path of the uvicorn

--- sample_projects/geoip/start.sh
+++ sample_projects/geoip/start.sh
@@ -4,4 +4,4 @@ HOST=`echo $1 | cut -d ":" -f 1`
 PORT=`echo $1 | cut -d ":" -f 2`
 export CONFIG_PATH=geoip.ini
 export APP_ENDPOINT=$1
-./venv/bin/uvicorn main:app --reload --host $HOST --port $PORT 
+uvicorn main:app --reload --host $HOST --port $PORT 
