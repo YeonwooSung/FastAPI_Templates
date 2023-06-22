e4454a25cc5d00cb96fe1ce1a5a2feb8a9333833
YeonwooSung <neos960518@gmail.com>
2023-06-22 17:18:16 +0900

    fix: Fix typos

--- sample_projects/fastapi-movie-api/MovieAPI/api/schemas/__init__.py
+++ sample_projects/fastapi-movie-api/MovieAPI/api/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .movie_actor_schema import Actor, ActorInput, Movie, MovieActorLink, MovieInput, MovieOutput
-from .subscription_schema import Subscription, SubscriptionInput, SubscriptionOutput
+from .subscription_schema import Subscription, SubscriptionInput
 from .token_schema import Token
 from .user_schema import User, UserInput
