62a1e0dc2dab0e42b04ea120e686a63f4ceaac1c
YeonwooSung <neos960518@gmail.com>
2024-08-17 22:21:01 +0900

    feat: Add sample codes for bulkhead pattern (context ver and decorator ver)

---
+++ backend_essentials/bulkhead/bulkhead_context.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from hyx.bulkhead import bulkhead
+
+# app is an instance of framework like Flask or FastAPI
+app = FastAPI()
+
+limiter = bulkhead(max_concurrency=10, max_capacity=100)
+
+
+@app.post("/projects/")
+async def create_new_project():
+    """
+    Create a new project
+    """
+    async with limiter:
+        ...
---
+++ backend_essentials/bulkhead/bulkhead_decorator.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from hyx.bulkhead import bulkhead
+
+# app is an instance of framework like Flask or FastAPI
+app = FastAPI()
+
+
+@app.post("/projects/")
+@bulkhead(max_concurrency=10, max_capacity=100)
+def create_new_project():
+    """
+    Create a new project
+    """
+    ...
