dd87a231a220de1c8dcaff402fbf648ad69bb4a8
Yeonwoo Sung <neos960518@gmail.com>
2025-02-04 11:08:50 +0900

    Handle event-driven websocket more efficiently

--- basic-functionality/redis_pubsub_websocket/main.py
+++ basic-functionality/redis_pubsub_websocket/main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from fastapi import FastAPI, Depends
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
 import redis.asyncio as redis
 
 app = FastAPI()
@@ -29,19 +29,25 @@ async def ws_root(websocket: WebSocket, rdb: redis.Redis = Depends(redis_connect
     async def listen_redis():
         ps = rdb.pubsub()
         await ps.psubscribe("test_channel")
-        while True:
-            message = await ps.get_message(ignore_subscribe_messages=True, timeout=None)
-            if message is None:
-                continue
-            text_message = message['data'].decode('utf-8')
-            if text_message == "stop":
-                await websocket.send_text("closing the connection")
-                break
-            await websocket.send_text(text_message)
+
+        # use try-catch for WebSocketDisconnect event
+        try:
+            while True:
+                message = await ps.get_message(ignore_subscribe_messages=True, timeout=None)
+                if message is None:
+                    continue
+                text_message = message['data'].decode('utf-8')
+                if text_message == "stop":
+                    await websocket.send_text("closing the connection")
+                    break
+                await websocket.send_text(text_message)
+        except WebSocketDisconnect:
+            pass
 
     async def listen_ws():
-        while True:
-            message = await websocket.receive_text()
+        # Use 'async for' instead of 'while True' on Websocket
+        # Reference: <https://github.com/Kludex/fastapi-tips?tab=readme-ov-file#3-use-async-for-instead-of-while-true-on-websocket>
+        async for message in websocket.iter_text():
             await rdb.publish("test_channel", message)  # publishing the message to the redis pubsub channel
 
     await asyncio.wait([listen_ws(), listen_redis()], return_when=asyncio.FIRST_COMPLETED)
